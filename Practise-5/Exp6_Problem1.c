9a778a032eeabf1f9d9dc83b0c56dbfe8678799c
PanavPatel06 <panavpatel461906@gmail.com>
2025-09-01 14:43:25 +0530

    Updates

--- Practise-5/Exp6_Problem1.c
+++ Practise-5/Exp6_Problem1.c
@@ -3,7 +3,7 @@
 
 struct queue
 {
-    int f,r,size;
+    int f,r,size; // Here r is pointing to next address not the last element address
     int * array;
 };
 
@@ -12,15 +12,28 @@ struct queue * createQueue(int size)
     struct queue *q=(struct queue *)malloc(sizeof(struct queue));
     q->f=-1;
     q->r=-1;
+    q->size=size;
     q->array=(int *)malloc(sizeof(int)*size);
     return q;
 };
 
 int isEmpty(struct queue *q)
 {
-    if(F=-1)
+    if(q->f==-1)
     {
-        printf("The queue is empty");
+        return 0;
+    }
+    return 1;
+}
+
+int isFull(struct queue *q)
+{
+    if(q->r-1==q->size-1 && q->f==0)
+    {
+        return 0;
+    }
+    else if(q->f==(q->r-1)% q->size)
+    {
         return 0;
     }
     return 1;
@@ -31,40 +44,78 @@ int Enqueue(struct queue *q)
     int item;
     printf("Enter the number you want to insert=");
     scanf("%d",&item);
-    if(isEmpty(q)==1)
+    if(isEmpty(q)==0)
     {
-        q->f++;
-        q->r++;
+        q->f=0;
+        q->r=1;
         q->array[q->f]=item;
+        printf("Element inserted\n");
     }
-    else if(q->r<q->size-1)
+    else if(q->r<=q->size-1)
     {
         q->array[q->r]=item;
+        q->r++;
     }
     else
     {
         q->r=0;
+        q->array[q->r]=item;
+        q->r++;
     }
+    return 0;
+}
+
+int Dequeue(struct queue *q)
+{
+    if(isEmpty(q)==0)
+    {
+        printf("No element to delete/dequeue\n");
+    }
+    else if(q->f==q->size-1)
+    {
+        q->f=0;
+    }
+    else
+    {
+        printf("The element %d is deleted \n",q->array[q->f]);
+        q->f++;
+    }
+}
+
+int print(struct queue *q)
+{
+    printf("The front element is %d\n",q->array[q->f]);
+    printf("The rear element is %d\n",q->array[q->r-1]);
 }
 
 int main()
 {
     int size,i;
-    printf("Enter the size for queue=")
+    printf("Enter the size for queue=");
     scanf("%d",&size);
-    struct queue *q=createQueue(size)
+    struct queue *q=createQueue(size);
     printf("Enter a number for the following choice=\n");
     while(1)
     {
-        printf("1.To check for empty queue \n2.To check if queue is full \n3.To insert element \n4.To delete element \n5.To print front and rear element \n6.To exit");
+        printf("1.To check for empty queue \n2.To check if queue is full \n3.To insert element \n4.To delete element \n5.To print front and rear element \n6.To exit\n");
         scanf("%d",&i);
         switch(i)
         {
             case 1:
-                isEmpty(q);
+                if(isEmpty(q)==0)
+                {
+                    printf("The queue is empty\n");
+                }
+                else
+                    printf("The queue is not empty\n");
                 break;
             case 2:
-                isFull(q);
+                if(isFull(q)==0)
+                {
+                    printf("The queue is full\n");
+                }
+                else
+                    printf("The queue is not full\n");
                 break;
             case 3:
                 Enqueue(q);
@@ -78,6 +129,8 @@ int main()
             case 6:
                 exit(0);
                 break;
+            default:
+                printf("Invalid input");
         }
     }
 }
